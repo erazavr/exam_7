b82ceda8d2fe76c1f5274eaed1149e55e5c6c8d1
Ernur Kurmanbekov <erazavr85@gmail.com>
2019-11-23 15:29:21 +0600

    Экспортировал из ITEMS.js

---
+++ src/components/ITEMS/ITEMS.js
@@ -0,0 +1,16 @@
+const items = {
+    cheeseBurger: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Cheeseburger.png',
+    hamburger: 'https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Fast-Food-PNG-Clipart/Hamburger_PNG_Vector_Clipart_Picture.png?m=1434276763',
+    fries: 'http://pngimg.com/uploads/fries/fries_PNG17.png',
+    coffee: 'https://purepng.com/public/uploads/large/purepng.com-cup-mug-coffeecupcoffeebeanmug-1411527408007dldl0.png',
+    tea: 'https://pngriver.com/wp-content/uploads/2018/04/Download-Tea-PNG-File.png',
+    cola: 'http://assets.stickpng.com/thumbs/580b57fbd9996e24bc43c0de.png',
+};
+export const ITEMS = [
+    {name: 'Hamburger', price: 80, img: items.hamburger},
+    {name: 'Cheeseburger', price: 90, img: items.cheeseBurger},
+    {name: 'Fries', price: 45, img: items.fries},
+    {name: 'Coffee', price: 70, img: items.coffee},
+    {name: 'Tea', price: 50, img: items.tea},
+    {name: 'Cola', price: 50, img: items.cola},
+];
