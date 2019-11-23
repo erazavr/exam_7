a6d748c1812071325daf6eeb126d119f848f3fdf
Ernur Kurmanbekov <erazavr85@gmail.com>
2019-11-23 15:30:18 +0600

    Написал методы addFood и removeFood

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Items from "./components/Items/Items";
+import Order from "./components/Order/Order";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+    state = {
+        items: [
+            {name: 'Hamburger', count: 0, price: 80},
+            {name: 'Cheeseburger', count: 0, price: 90},
+            {name: 'Fries', count: 0, price: 45},
+            {name: 'Coffee', count: 0, price: 70},
+            {name: 'Tea', count: 0, price: 50},
+            {name: 'Cola', count: 0, price: 40},
+        ],
+        totalPrice: 0,
+    };
+    addFood = count => {
+        const items = [...this.state.items];
+        const itemsIndex = items.findIndex(t => t.name === count.name);
+        items[itemsIndex].count++;
+        let totalPrice = this.state.totalPrice;
+        totalPrice += count.price;
+        this.setState({items,totalPrice});
+    };
+    removeFood = name => {
+        const items = [...this.state.items];
+        const itemsIndex = items.findIndex(t => t.name === name.name);
+        items[itemsIndex].count--;
+        let totalPrice = this.state.totalPrice;
+        totalPrice -= name.price;
+        this.setState({items,totalPrice});
+    };
+    render() {
+        return (
+            <div className='mainBlock'>
+                <Order
+                    items={this.state.items}
+                    totalPrice = {this.state.totalPrice}
+                    remove={this.removeFood}
+                />
+
+                <div className='item'>
+                <h4>Add items</h4>
+                <Items
+                    food={this.addFood}
+                    itemA={this.state.items}/>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
